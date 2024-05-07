12ba5bd4e80ec2b27d7d05944759ee164b6cfcff
Schexcel <schexcel@gmail.com>
2024-05-07 15:56:53 +0200

    v1.5

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <nav-bar/>
+  <RouterView />
+</template>
+
+<script setup>
+import { RouterView } from 'vue-router';
+import NavBar from "@/components/layouts/NavBar.vue";
+
+</script>
+
+<style scoped>
+
+</style>
